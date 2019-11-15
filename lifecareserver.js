55d10bbc57b33d96cb0d64cff2b084acb24d9f67
Tom George <tom.george@metlife.com>
2019-11-15 15:45:02 +0900

    mock data path changed

--- lifecareserver.js
+++ lifecareserver.js
@@ -2,12 +2,12 @@ var express = require('express');
 var path = require('path');
 var app  = express();
 let fs = require('fs');
-var hospital=require('./data/hospitalcomponent.json');
-var hospitalDetails=require('./data/hospitaldetails.json');
+var hospital=require('./mock/hospitalcomponent.json');
+var hospitalDetails=require('./mock/hospitaldetails.json');
 let bodyParser = require('body-parser');
 let find = require('lodash/find');
 let isEmpty = require('lodash/isEmpty');
-let appointment=require('./data/appointment.json');
+let appointment=require('./mock/appointment.json');
 const PORT = process.env.PORT || 8282;
 
 const getDecodedString = (value) => {
@@ -36,12 +36,12 @@ function getRandomInt(){
 }
 app.post('/hospitals/createappointment',function(req,res){
   var user_ID = getRandomInt();
-  let appointment_object = JSON.parse(fs.readFileSync('./data/appointment.json', 'utf8'));
+  let appointment_object = JSON.parse(fs.readFileSync('./mock/appointment.json', 'utf8'));
   let appointment_data = '{"doctor":{"patient": [' +
       '{ "userId":" ' + user_ID  + ' ","patientname":" '+ req.body.name +' " ,"patientage":" '+ req.body.age +' " ,"patientsex":" '+ req.body.sex +' " , "contactno":" '+ req.body.number +'  ","patientdate":" '+ req.body.date +' " ,"patientslot":" '+ req.body.slot +' " }]}}';
   let Tem = JSON.parse(appointment_data);
   appointment_object.doctor.patient.push(Tem.doctor.patient[0]);
-  fs.writeFile ('./data/appointment.json',JSON.stringify(appointment_object , null, 2) , function(err) {
+  fs.writeFile ('./mock/appointment.json',JSON.stringify(appointment_object , null, 2) , function(err) {
     // console.log("callback::running");
     if (err){
       console.log("Error is::"+err+"...................");
@@ -55,12 +55,12 @@ app.post('/hospitals/createappointment',function(req,res){
 
 app.post('/hospitals/createHospitals',function(req,res){
   var lookupid = getRandomInt();
-  let create_object = JSON.parse(fs.readFileSync('./data/HospitalComponent.json', 'utf8'));
+  let create_object = JSON.parse(fs.readFileSync('./mock/HospitalComponent.json', 'utf8'));
   let create_data = '{"cardContent":{"relationItems": [' +
       '{ "lookupid":" ' + lookupid  + ' " ,"name":" '+ req.body.name +' ","address":" '+ req.body.address +' " ,"aboutUs":" '+ req.body.aboutUs +' " , "contactUs":" '+ req.body.contactUs +'  " }]}}';
   let Tem = JSON.parse(create_data);
   create_object.cardContent.relationItems.push(Tem.cardContent.relationItems[0]);
-  fs.writeFile ('./data/HospitalComponent.json',JSON.stringify(create_object , null, 2) , function(err) {
+  fs.writeFile ('./mock/HospitalComponent.json',JSON.stringify(create_object , null, 2) , function(err) {
     console.log("callback:: create running");
     if (err){
       console.log("Error is::"+err+"...................");
@@ -73,7 +73,7 @@ app.post('/hospitals/createHospitals',function(req,res){
 
 app.post('/users/login',function(req,res){
   console.log(JSON.stringify(req.body));
-  let usersCollection = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'));
+  let usersCollection = JSON.parse(fs.readFileSync('./mock/users.json', 'utf8'));
   const username = getDecodedString(req.body.username);
   const password = getDecodedString(req.body.password);
   const user = find(usersCollection.users, {'username' : username, 'password': password});  
@@ -87,12 +87,12 @@ app.post('/users/login',function(req,res){
 
 app.post('/users/register',function(req,res){
   console.log(JSON.stringify(req.body));
-  let usersCollection = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'));
+  let usersCollection = JSON.parse(fs.readFileSync('./mock/users.json', 'utf8'));
   const username = req.body.username;
   const password = getDecodedString(req.body.password);
   usersCollection.users.push({'username' : username, 'password': password});  
   console.log('info', 'Registered user:', usersCollection);
-  fs.writeFile ('./data/users.json',JSON.stringify(usersCollection , null, 2) , function(err) {
+  fs.writeFile ('./mock/users.json',JSON.stringify(usersCollection , null, 2) , function(err) {
     if (err){
       console.log("Register User Error | "+err);
       res.header('Access-Control-Allow-Origin','*').status(400).json({registerSuccess : false, status : 400});      
